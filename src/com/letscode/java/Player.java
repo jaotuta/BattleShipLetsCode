cc005cffb8fac04fda29ff6d9c98d3a54d88168d
Joao Lucas <jaotuta@hotmail.com>
2022-01-03 22:16:33 -0300

    dev

--- src/com/letscode/java/Player.java
+++ src/com/letscode/java/Player.java
@@ -4,23 +4,33 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Player {
-    int tiroPosUmPlayer = 0;
-    int tiroPosDoisPlayer = 0;
-    int tiroPosUmPc = 0;
-    int tiroPosDoisPc = 0;
+    int tiroPosUmPlayer;
+    int tiroPosDoisPlayer;
+    int tiroPosUmPc;
+    int tiroPosDoisPc;
+
+    String[][] historicoPlayer = new String[3][3];
+    String[][] historicoPc = new String[3][3];
+
 
     Scanner numberScanner = new Scanner(System.in);
     Random numberGeneration = new Random();
 
 
     public void jogadaPlayer() {
-        System.out.println("Certo Capitão, nossa vez de atirar, qual a primeira cordenada?(0 - 3) ");
+
+        System.out.println("Certo Capitão, nossa vez de atirar, qual a primeira cordenada?(0 - 2) ");
         tiroPosUmPlayer = numberScanner.nextInt();
-        System.out.println("Agora precisamos da segunda cordenada?(0 - 3) ");
+        System.out.println("Agora precisamos da segunda cordenada?(0 - 2) ");
         tiroPosDoisPlayer = numberScanner.nextInt();
+        historicoPlayer[tiroPosUmPlayer][tiroPosDoisPlayer] = "X";
     }
+
+
     public void jogadaPc() {
         tiroPosUmPc = numberGeneration.nextInt(2);
         tiroPosDoisPc = numberGeneration.nextInt(2);
+        historicoPc[tiroPosUmPc][tiroPosDoisPc] = "X";
+
     }
 }
